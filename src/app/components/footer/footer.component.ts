4e5620075c55c31f29dc4fc8821cc0fb0513920e
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-01 19:17:16 +0200

    022 added footer markup

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+interface Address {
+  info:string;
+  email:string;
+  location:string;
+  phone:string
+}
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  public footer__address: Address = {
+    info:'Billing & General Support',
+    email:'hello@xtrades.net',
+    location:'Los Angeles, CA 90028',
+    phone:'1-530-723-5499'
+  }
+  protected readonly location = location;
 }
