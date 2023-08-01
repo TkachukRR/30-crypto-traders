14b26c67c4586eed40571f170e43ce20dc5e36a7
Roman Tkachuk <tkachukrr@gmail.com>
2023-08-01 20:24:02 +0200

    022 styled footer

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private _isAuthenticated = false;
+  private _isAuthenticated = true;
 
   public login():void{
     this._isAuthenticated = true;
